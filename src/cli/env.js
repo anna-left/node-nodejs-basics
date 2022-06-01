bffd0d94565c0023cb5301110beb63c63bf8d852
anna-left <anna_left@mail.ru>
2022-06-01 17:15:44 +0300

    feat: solve the task env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,22 @@
+import { fsOperationFailed, sendMessage } from '../global/globalFunctions.js';
+
 export const parseEnv = () => {
-    // Write your code here 
+    let env = process.env;
+
+    env['RSS_1.NAME'] = 'The Rolling Scopes School';
+    env['RSS_2.COURSE'] = 'Node.js';
+    env['RSS_3.TASK'] = 'Node.js basics';
+    env['RSS_4.STUDENT'] = 'anna_left';
+    env['RSS_5.SCORE'] = '206 :-)';
+    sendMessage('environment variables with prefix RSS_');
+    
+    for (let key in env) {
+        if (key.indexOf('RSS_') === 0) {
+            console.log(`${key} === ${env[key]}`);
+        }
+        
+    }
+    // console.log( process.argv);
 };
+
+parseEnv();
