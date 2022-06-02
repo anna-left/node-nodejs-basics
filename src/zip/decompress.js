b072cd2ceac1a11d6ee9c87e6de6ae2f04b8dd80
anna-left <anna_left@mail.ru>
2022-06-02 20:45:04 +0300

    feat: solve the task zip/decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,26 @@
+import path from 'path';
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream } from 'fs';
+
+import { sendMessage } from '../global/globalFunctions.js';
+
 export const decompress = async () => {
-    // Write your code here 
+    const __dirname = path.resolve();
+    const fileName = path.join(__dirname, 'files/fileToCompress.txt');
+    const fileArchiveName = path.join(__dirname, 'files/archive.gz');
+    const gzip = createUnzip();
+    const readStream = createReadStream(fileArchiveName);
+    const writeStream = createWriteStream(fileName);
+
+    pipeline(readStream, gzip, writeStream, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        } else {
+            sendMessage('File is decompressed');
+        }
+    });
 };
+
+decompress();
