24627a6cea73032bc99ad7d1ea3a0de987eabb27
anna-left <anna_left@mail.ru>
2022-06-02 15:34:23 +0300

    feat: solve the task stream/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+import { sendMessage } from '../global/globalFunctions.js';
+
 export const write = async () => {
-    // Write your code here 
+    const __dirname = path.resolve();
+    const fileName = path.join(__dirname, 'files/fileToWrite.txt');
+    const writeStream = fs.createWriteStream(fileName);
+
+    sendMessage('Please enter data');
+
+    process.stdin.on('data', data => {
+        writeStream.write(data.toString());
+        sendMessage('File written');
+        process.exit();
+    });
+
+
 };
+
+write();
