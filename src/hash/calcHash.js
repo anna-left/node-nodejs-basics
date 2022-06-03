3fb39befb576b5f4ec948c6bd2a7753bfeddc134
anna-left <anna_left@mail.ru>
2022-06-03 20:42:55 +0300

    feat: solve the task calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,22 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+
+import { sendMessage } from '../global/globalFunctions.js';
+
+const __dirname = path.resolve();
+const fileName = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
+
 export const calculateHash = async () => {
-    // Write your code here 
+
+    const fileBuffer = fs.readFileSync(fileName);
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(fileBuffer);
+
+    const hex = hashSum.digest('hex');
+
+    sendMessage('SHA256 hash as hex');
+    console.log(hex); 
 };
+
+calculateHash();
