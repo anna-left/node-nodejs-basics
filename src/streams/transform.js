5f57b40de8664331f394a2bea5f27bbdaee3fb8f
anna-left <anna_left@mail.ru>
2022-06-04 08:47:40 +0300

    feat: solve the task transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,29 @@
+import { Transform, pipeline } from 'stream';
+
+import { sendMessage } from '../global/globalFunctions.js';
+
 export const transform = async () => {
-    // Write your code here 
+    const stdin = process.stdin;
+    const stdout = process.stdout;
+
+    sendMessage('Please enter data for transformation');
+
+    const transformReverse = new Transform({
+        transform(chunk, enc, cb) {
+            const trimChunk = chunk.toString().trim();
+            const reversedChunk = trimChunk.split("").reverse().join("");
+            this.push(reversedChunk + '\n');
+            cb();
+        }
+    })
+    pipeline(
+        stdin,
+        transformReverse,
+        stdout,
+        error => {
+            console.log(`Error: ${error}`);
+        }
+    )
 };
+
+transform();
