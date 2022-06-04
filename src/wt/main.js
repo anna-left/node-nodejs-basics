acd7ff6c6c39c0363513977f215291f3b00465e5
anna-left <anna_left@mail.ru>
2022-06-04 08:42:51 +0300

    feat: solve the task main, worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,33 @@
+import { Worker } from 'worker_threads';
+import fs from 'fs';
+import path from 'path';
+import os from 'os';
+
+import { sendMessage } from '../global/globalFunctions.js';
+
+const cpuCount = os.cpus().length;
+
+const __dirname = path.resolve();
+const fileName = path.join(__dirname, 'worker.js');
+
 export const performCalculations = async () => {
-    // Write your code here
+    const resultPromises = [];
+    for (let i = 0; i < cpuCount; i++) {
+        const promise = new Promise((resolve, reject) => {
+            const worker = new Worker(fileName, {
+                workerData: 10 + i
+            });
+            worker.on('message', (res) => {
+                resolve(res);
+            });
+        })
+        resultPromises.push(promise);
+    }
+    sendMessage('Result of the computation from workers');
+    const resPromise = await Promise.all(resultPromises);
+    for (let i = 0; i < resPromise.length; i++) {
+        console.log(resPromise[i]);
+    }
 };
+
+performCalculations();
