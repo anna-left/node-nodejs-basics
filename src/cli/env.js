e1745e73650a678c46652d0a6b739d71de45a9d6
anna-left <anna_left@mail.ru>
2022-06-04 21:15:51 +0300

    feat: use  command line arguments in script env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,18 +1,36 @@
 import { sendMessage } from '../global/globalFunctions.js';
 
 export const parseEnv = () => {
-    let env = process.env;
+    const env = process.env;
 
     env['RSS_1.NAME'] = 'The Rolling Scopes School';
     env['RSS_2.COURSE'] = 'Node.js';
     env['RSS_3.TASK'] = 'Node.js basics';
     env['RSS_4.STUDENT'] = 'anna_left';
     env['RSS_5.SCORE'] = '206 :-)';
+
+    const argv = process.argv;
+
+    let curFlag = '';
+    for (let key in argv) {
+        if (argv[key].indexOf('--')) {
+            if (curFlag) {
+                if (!curFlag.indexOf('RSS_')) {
+                    env[curFlag] = argv[key];
+                }
+                
+            }
+            curFlag = '';
+        } else {
+            curFlag = argv[key].slice(2);
+        }
+    }
+
     sendMessage('Environment variables with prefix RSS_');
     
     for (let key in env) {
-        if (key.indexOf('RSS_') === 0) {
-            console.log(`${key} === ${env[key]}`);
+        if (!key.indexOf('RSS_')) {
+            console.log(`${key} = ${env[key]}`);
         }
         
     }
