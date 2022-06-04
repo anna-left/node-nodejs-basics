189a1d93d7e64911fcd5e5abb94978dd2936b920
anna-left <anna_left@mail.ru>
2022-06-04 15:55:14 +0300

    feat: solve the task cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,13 @@
+import cp from 'child_process';
+
+import { fsOperationFailed, sendMessage } from '../global/globalFunctions.js';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = cp.spawn('node', ['./files/script.js', 'arg1', 'arg2']);
+    sendMessage('Creating a child process');
+    child.stdin.setEncoding('utf-8');
+    child.stdout.pipe(process.stdout);
+    process.stdin.pipe(child.stdin);
 };
+
+spawnChildProcess();
